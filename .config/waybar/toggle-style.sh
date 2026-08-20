f39ee3e33fbfc120be6d7f30e79877a84159bfa1
Axkeya <axkeya@gmail.com>
2026-08-20 09:23:16 +0500

    README.md update 2

--- .config/waybar/toggle-style.sh
+++ .config/waybar/toggle-style.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
 CONFIG_LINK="$SCRIPT_DIR/config.jsonc"
-STYLE_LINK="$SCRIPT_DIR/style.css"
 
 CURRENT_CONFIG=$(readlink -f "$CONFIG_LINK")
 
 cd "$SCRIPT_DIR" || exit 1
 
 if [[ "$CURRENT_CONFIG" == *"config-minimal.jsonc"* ]]; then
-    ln -sf config-rounded.jsonc config.jsonc
-    ln -sf style-rounded.css style.css
-    echo "Switched to rounded"
+    ln -sf config-detailed.jsonc config.jsonc
+    ln -sf style-detailed.css style.css
+    echo "Switched to detailed"
 else
     ln -sf config-minimal.jsonc config.jsonc
     ln -sf style-minimal.css style.css
--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Includes multiple layouts togglable on the fly via `toggle-style.sh`.
 | Minimal Style |
 | :--- |
 | ![Minimal Bar](./assets/waybar_mode_2.png) |
+
+| Detailed Style |
+| :--- |
+| ![Detailed Bar](./assets/waybar_mode_1.png) |
