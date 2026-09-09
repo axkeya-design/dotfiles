69137d7e355a10fc43ce7d52558bf734c9ea779f
Axkeya <axkeya@gmail.com>
2026-09-09 18:05:48 +0500

    Fish config update (add s-ddrg)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ if status is-interactive
     alias v="vim"
     alias yz="yazi"
 
+	alias s-ddgr="s -b w3m -p duckduckgo"
+
     alias mpv-term="mpv -vo=tct --really-quiet"
     alias mpv-play="mpv --no-video --really-quiet"
 
