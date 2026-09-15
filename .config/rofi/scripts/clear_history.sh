3edd52e0fca5253838b27d6ff8d7b186af376070
Axkeya <axkeya@gmail.com>
2026-09-15 17:55:35 +0500

    rofi cache update

--- .config/rofi/scripts/clear_history.sh
+++ .config/rofi/scripts/clear_history.sh
@@ -4,5 +4,6 @@ rm -f "$HOME/.local/share/rofi/rofi_calc_history"
 rm -f "$HOME/.cache/rofi-"*
 rm -f "$HOME/.local/share/rofi/rofi_history"
 rm -f "/tmp/rofi_wallpaper_list"
+rm -rf "$HOME/.local/share/rofimoji"
 
 notify-send "Rofi" "History and cache cleared successfully! 󰃢" --icon=dialog-information
